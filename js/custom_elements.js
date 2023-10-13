9e3f621f4f6cc3a682068204664eaf8af148c3b5
user <nicholas.daultry@gmail.com>
2023-10-13 10:31:38 +0100

    Used javascript for headers and footers

---
+++ js/custom_elements.js
@@ -0,0 +1,41 @@
+class AppHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <nav>
+            <a href="index.html">Home</a>
+            <a href="whats_new.html">Whats new</a>
+            <a href="publications.html">Publications</a>
+            <a href="projects.html">Projects</a>
+        </nav>
+        `
+    }
+}
+
+class Footer extends HTMLElement { 
+    connectedCallback() {
+        this.innerHTML =  `
+        <footer>
+            <hr>    
+            <p>Email: nicholas [dot] daultryball [at] maths [dot] ox [dot] ac [dot] uk</p>
+            <p><a href = "https://www.linkedin.com/in/nicholas-daultry-ball-85a53011a/">Linkedin</a></p> 
+            <p><a href="docs/Resume_CV.pdf" attributes-list download="CV Nicholas Daultry Ball">CV</a></p>
+            <div>
+                <img 
+                    src="docs/oxford_logo.png"
+                    height=50 
+                >
+                <img 
+                    src="docs/cdt_logo.jpg"
+                    height=50
+                >
+            </div>
+        </footer>
+    `
+    }
+}
+
+window.customElements.define('nav-bar', AppHeader)
+
+window.customElements.define('custom-footer', Footer) 
+    
+   
